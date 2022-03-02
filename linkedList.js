2f9c58840b6ece23239b7802a3e375e32c606fbc
Dylmarcor <dylancorbett@icloud.com>
2022-03-02 11:12:12 -0800

    add method on linked list

--- linkedList.js
+++ linkedList.js
@@ -8,3 +8,28 @@ class Node {
     }
 }
 
+// Create Linked List Class
+class LinkedList {
+    constructor() {
+        this.head = null; // first Node in list
+        this.size = 0;  // number of Nodes in list
+    }
+
+    add(val) {
+        let node = new Node(val); // create a new Node
+
+        let current;  // for storing current Node
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+}
