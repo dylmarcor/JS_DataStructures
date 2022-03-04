6ca5b28f46624e6c68c355b4558412f3fdebd1b2
Dylmarcor <dylancorbett@icloud.com>
2022-03-03 19:04:22 -0800

    add node to binary sort

---
+++ hierarchical/binary_search_tree.js
@@ -0,0 +1,19 @@
+// Simple Binary Sort Tree
+
+// Create Node Obj 
+class Node {
+    constructor(element) {
+        this.head = element;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySort {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    
+}
---
+++ linear_structures/tests/linked_list_test.js
@@ -0,0 +1,3 @@
+import { LinkedList } from "../linkedList";
+
+let list_test = new LinkedList;
